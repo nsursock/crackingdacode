511f2882abef0a8b17a92d2a6111946ac48dc01b
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-06-27 19:38:42 +0300

    added stripe payment

--- src/_data/variables.js
+++ src/_data/variables.js
@@ -1,3 +1,7 @@
 module.exports = {
   environment: process.env.NODE_ENV || "development",
+  stripeKey:
+    process.env.ELEVENTY_STRIPE_KEY ||
+    "pk_test_51IHPrGGrVUCQrQF7GzGUqErBudr5gE6HScm57Et46MkllKNJ9oEkNhghGdPjnlzpOchCOSylaR2paK85hwIVhAbn00Kgmxuzyl",
+  apiUrl: process.env.ELEVENTY_BACKEND_URL || "http://localhost:8081",
 };
