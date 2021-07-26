21bb322e6bb707c19332268352d8c876a6744281
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-07-26 04:37:06 +0300

    used another starter template

--- .eleventy.js
+++ .eleventy.js
@@ -1,67 +1,28 @@
 const yaml = require("js-yaml");
-const { DateTime } = require("luxon");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const htmlmin = require("html-minifier");
-// require("dotenv").config();
 
-module.exports = function (eleventyConfig) {
-  // Disable automatic use of your .gitignore
-  eleventyConfig.setUseGitIgnore(false);
-
-  // Merge data instead of overriding
-  eleventyConfig.setDataDeepMerge(true);
-
-  // human readable date
-  eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
-      "dd LLL yyyy"
-    );
+module.exports = (config) => {
+  config.addPassthroughCopy({ public: "./" });
+  config.setBrowserSyncConfig({
+    files: ["dist/**/*"],
+    open: true,
+    // Tweak for Turbolinks & Browserstack Compatibility
+    snippetOptions: {
+      rule: {
+        match: /<\/head>/i,
+        fn: function (snippet, match) {
+          return snippet + match;
+        },
+      },
+    },
   });
+  config.setDataDeepMerge(true);
+  config.addDataExtension("yaml", (contents) => yaml.load(contents));
+  config.addPassthroughCopy("./src/static/img");
 
-  // Syntax Highlighting for Code blocks
-  eleventyConfig.addPlugin(syntaxHighlight);
-
-  // To Support .yaml Extension in _data
-  // You may remove this if you can use JSON
-  eleventyConfig.addDataExtension("yaml", (contents) =>
-    yaml.safeLoad(contents)
-  );
-
-  // Copy Static Files to /_Site
-  eleventyConfig.addPassthroughCopy({
-    "./src/admin/config.yml": "./admin/config.yml",
-    "./node_modules/alpinejs/dist/cdn.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
-      "./static/css/prism-tomorrow.css",
-  });
-
-  // Copy Image Folder to /_site
-  eleventyConfig.addPassthroughCopy("./src/static/img");
-  eleventyConfig.addPassthroughCopy("./src/static/font");
-
-  // Copy favicon to route of /_site
-  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
-
-  // Minify HTML
-  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-    if (outputPath.endsWith(".html")) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-      });
-      return minified;
-    }
-
-    return content;
-  });
-
-  // Let Eleventy transform HTML files as nunjucks
-  // So that we can use .html instead of .njk
   return {
     dir: {
       input: "src",
+      output: "dist",
     },
     htmlTemplateEngine: "njk",
   };
---
+++ src/_bundle/main.js
@@ -0,0 +1,34 @@
+import Alpine from "alpinejs";
+import "htmx.org";
+import "lazysizes";
+import intersect from "@alpinejs/intersect";
+
+window.Alpine = Alpine;
+
+// Start Alpine when the page is ready.
+window.addEventListener("DOMContentLoaded", () => {
+  Alpine.plugin(intersect);
+  Alpine.start();
+});
+
+// Restart Alpine when the DOM is altered by HTMX.
+document.body.addEventListener("htmx:afterSwap", () => {
+  Alpine.plugin(intersect);
+  Alpine.start();
+});
+
+// Basic Store Example in Alpine.
+window.addEventListener("alpine:initializing", () => {
+  Alpine.store("nav", {
+    isOpen: false,
+    close() {
+      return (this.isOpen = false);
+    },
+    open() {
+      return (this.isOpen = true);
+    },
+    toggle() {
+      return (this.isOpen = !this.isOpen);
+    },
+  });
+});
