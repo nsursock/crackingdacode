adb3ab6feb01f1f3e6fbd236f500503fe3bf7ab8
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-08-02 09:40:02 +0300

    made own intersect directive

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,33 @@
-const yaml = require("js-yaml");
+const yaml = require('js-yaml')
 
 module.exports = (config) => {
-  config.addPassthroughCopy({ public: "./" });
+  config.addPassthroughCopy({ public: './' })
   config.setBrowserSyncConfig({
-    files: ["dist/**/*"],
+    files: ['dist/**/*'],
     open: true,
     // Tweak for Turbolinks & Browserstack Compatibility
     snippetOptions: {
       rule: {
         match: /<\/head>/i,
         fn: function (snippet, match) {
-          return snippet + match;
+          return snippet + match
         },
       },
     },
-  });
-  config.setDataDeepMerge(true);
-  config.addDataExtension("yaml", (contents) => yaml.load(contents));
-  config.addPassthroughCopy("./src/static/img");
+  })
+  config.setDataDeepMerge(true)
+  config.addDataExtension('yaml', (contents) => yaml.load(contents))
+
+  // config.addPassthroughCopy({
+  //   './assets/main.bundle.js': './static/js/main.bundle.js',
+  // })
+  config.addPassthroughCopy('./src/static/img')
 
   return {
     dir: {
-      input: "src",
-      output: "dist",
+      input: 'src',
+      output: 'dist',
     },
-    htmlTemplateEngine: "njk",
-  };
-};
+    htmlTemplateEngine: 'njk',
+  }
+}
--- src/_bundle/main.js
+++ src/_bundle/main.js
@@ -1,34 +1,60 @@
-import Alpine from "alpinejs";
-import "htmx.org";
-import "lazysizes";
-import intersect from "@alpinejs/intersect";
+import Alpine from 'alpinejs'
+import 'htmx.org'
+import 'lazysizes'
+// import intersect from '@alpinejs/intersect'
 
-window.Alpine = Alpine;
+Alpine.directive(
+  'intersect',
+  (el, { expression, modifiers }, { evaluateLater, cleanup }) => {
+    let evaluate = evaluateLater(expression)
+
+    let observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        // if (entry.intersectionRatio === 0) return
+        // entry.target.style.opacity = entry.intersectionRatio;
+
+        console.log(entry.intersectionRatio)
+        if (entry.isIntersecting)
+          evaluate()
+
+        // modifiers.includes('once') && observer.disconnect()
+      })
+    })
+
+    observer.observe(el)
+
+    cleanup(() => {
+      observer.disconnect()
+    })
+  }
+)
+
+window.Alpine = Alpine
 
 // Start Alpine when the page is ready.
-window.addEventListener("DOMContentLoaded", () => {
-  Alpine.plugin(intersect);
-  Alpine.start();
-});
+window.addEventListener('DOMContentLoaded', () => {
+  // Alpine.plugin(intersect)
+  Alpine.start()
+})
 
 // Restart Alpine when the DOM is altered by HTMX.
-document.body.addEventListener("htmx:afterSwap", () => {
-  Alpine.plugin(intersect);
-  Alpine.start();
-});
+document.body.addEventListener('htmx:afterSwap', () => {
+  // Alpine.plugin(intersect)
+  Alpine.start()
+})
 
 // Basic Store Example in Alpine.
-window.addEventListener("alpine:initializing", () => {
-  Alpine.store("nav", {
+window.addEventListener('alpine:initializing', () => {
+  Alpine.store('nav', {
     isOpen: false,
     close() {
-      return (this.isOpen = false);
+      return (this.isOpen = false)
     },
     open() {
-      return (this.isOpen = true);
+      return (this.isOpen = true)
     },
     toggle() {
-      return (this.isOpen = !this.isOpen);
+      return (this.isOpen = !this.isOpen)
     },
-  });
-});
+  })
+})
