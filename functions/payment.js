785f2bd0949c80a490d6b340a1d9312387d11a67
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-09-26 10:27:56 +0300

    added conversion tracking for contact

--- functions/payment.js
+++ functions/payment.js
@@ -1,34 +1,34 @@
-const stripe = require("stripe")(process.env.EXPRESS_STRIPE_KEY ||
-    'sk_test_51JFiNrAfmGj15aPlIe0B1WTAbiChENOkQnnOfdrL8TnGuplrVLnxq18XrvAQn3HMLEqB09SqL2deOyI9wFTaEpEd00IUUpKLKh');
-const YOUR_DOMAIN = process.env.EXPRESS_FRONTEND_URL || 'http://localhost:8888';
+const stripe = require('stripe')(
+  process.env.EXPRESS_STRIPE_KEY ||
+    'sk_test_51JFiNrAfmGj15aPlIe0B1WTAbiChENOkQnnOfdrL8TnGuplrVLnxq18XrvAQn3HMLEqB09SqL2deOyI9wFTaEpEd00IUUpKLKh'
+)
+const YOUR_DOMAIN = process.env.EXPRESS_FRONTEND_URL || 'http://localhost:8888'
 
 exports.handler = async function (event, context) {
-    const { currency, amount } = JSON.parse(event.body);
-    
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: [
-          {
-            price_data: {
-              currency: currency,
-              product_data: {
-                name: "WebDev - Nicolas Sursock",
-                images: ["https://source.unsplash.com/500x500/?abstract"],
-              },
-              unit_amount: amount * 100,
-            },
-            quantity: 1,
-          },
-        ],
-        mode: "payment",
-        success_url: `${YOUR_DOMAIN}/thanks`,
-        cancel_url: `${YOUR_DOMAIN}`,
-      });
-    
-    //   res.json({ id: session.id });
+  const { currency, amount } = JSON.parse(event.body)
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ id: session.id })
-    };
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: currency,
+          product_data: {
+            name: 'WebDev - Nicolas Sursock',
+            images: ['https://source.unsplash.com/500x500/?abstract'],
+          },
+          unit_amount: amount * 100,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}/thanks`,
+    cancel_url: `${YOUR_DOMAIN}`,
+  })
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ id: session.id }),
+  }
 }
