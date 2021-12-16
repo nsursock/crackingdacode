81b5f5491196802e52191e8e6ba2cb3e4dd88640
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-12-16 12:35:52 +0100

    fixed stripe session problem

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,8 +1,5 @@
 module.exports = {
   devMode: process.env.NODE_ENV !== 'production',
   environment: process.env.NODE_ENV || 'development',
-  stripeKey: (process.env.NODE_ENV || 'development').startsWith('prod')
-    ? process.env.ELEVENTY_STRIPE_KEY
-    : 'pk_test_51JFiNrAfmGj15aPl4snuD3ohWeHiF3PzDmm6EIjS2kr4CV0Bx2eJZWIX432GBZ7EqpG5xmc68pIg0uh5kEIZOdcd00jdzhZBLa',
-  // apiUrl: process.env.ELEVENTY_BACKEND_URL || 'http://localhost:8081',
+  stripeKey: process.env.ELEVENTY_STRIPE_KEY
 }
