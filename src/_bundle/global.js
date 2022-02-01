370c15cc5a9a46d37662624bfb9ffe05f0b5f053
Nicolas Sursock <nicolas.sursock@gmail.com>
2022-02-01 21:20:24 +0100

    added circular progress

--- src/_bundle/global.js
+++ src/_bundle/global.js
@@ -3,6 +3,8 @@ export default () => ({
   landingPageOffset: 400,
   lastScrollTop: window.pageYOffset || document.documentElement.scrollTop,
   isScrollingUp: false,
+  circumference: 30 * 2 * Math.PI,
+  percent: 0,
   init() {
     window.addEventListener(
       'scroll',
@@ -10,6 +12,25 @@ export default () => ({
         var st = window.pageYOffset || document.documentElement.scrollTop
         this.isScrollingUp = st <= this.lastScrollTop
         this.lastScrollTop = st <= 0 ? 0 : st // for mobile or negative scrolling
+
+        const target = document.querySelector('#article')
+        if (target) {
+          const winTop =
+            window.pageYOffset || document.documentElement.scrollTop
+          const targetBottom = target.offsetTop + target.scrollHeight
+          const windowBottom = winTop + window.outerHeight
+          this.percent = Math.min(
+            Math.round(
+              100 -
+                ((targetBottom - windowBottom + window.outerHeight / 3) /
+                  (targetBottom -
+                    window.outerHeight +
+                    window.outerHeight / 3)) *
+                  100
+            ),
+            100
+          )
+        }
       },
       false
     )
