3cc3f8d00b731efee27a91bc9a1ac99e5d122a40
Nicolas Sursock <nicolas.sursock@gmail.com>
2022-06-01 06:48:06 +0200

    removed dependency to plausible

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-const { devMode } = require('./src/_data/env')
+const { devMode, statPwd } = require('./src/_data/env')
 // const { Appsignal } = require('@appsignal/nodejs')
 
 // const appsignal = new Appsignal({
@@ -35,34 +35,34 @@ module.exports = (config) => {
     return str[0].toUpperCase() + str.substring(1)
   }
 
-  config.addCollection('stats', async function (collection) {
-    let data = await fetch(
-      'https://plausible.io/api/v1/stats/aggregate?site_id=nsursock.netlify.app&period=6mo&metrics=visitors,pageviews,bounce_rate,visit_duration',
-      {
-        method: 'GET',
-        headers: {
-          // Authorization: `Bearer ${process.env.PLAUSIBLE_KEY}`,
-          Authorization: `Bearer 2K985BK3VVcjSZIpdUo6mbWti2118_DoDvoi9k2nxxZwMPoOmrYFYpARCPUhbR97`,
-        },
-      }
-    )
-    let json = (await data.json()).results
-    let stats = []
-    for (const prop in json) {
-      stats.push({
-        name: prop
-          .split('_')
-          .map((w) => capitalize(w))
-          .join(' '),
-        value: json[prop].value,
-      })
-    }
-    stats.push({
-      name: 'Views / Visitor',
-      value: (json.pageviews.value / json.visitors.value).toFixed(2),
-    })
-    return stats
-  })
+  // config.addCollection('stats', async function (collection) {
+  //   let data = await fetch(
+  //     'https://plausible.io/api/v1/stats/aggregate?site_id=nsursock.netlify.app&period=6mo&metrics=visitors,pageviews,bounce_rate,visit_duration',
+  //     {
+  //       method: 'GET',
+  //       headers: {
+  //         // Authorization: `Bearer ${process.env.PLAUSIBLE_KEY}`,
+  //         Authorization: `Bearer 2K985BK3VVcjSZIpdUo6mbWti2118_DoDvoi9k2nxxZwMPoOmrYFYpARCPUhbR97`,
+  //       },
+  //     }
+  //   )
+  //   let json = (await data.json()).results
+  //   let stats = []
+  //   for (const prop in json) {
+  //     stats.push({
+  //       name: prop
+  //         .split('_')
+  //         .map((w) => capitalize(w))
+  //         .join(' '),
+  //       value: json[prop].value,
+  //     })
+  //   }
+  //   stats.push({
+  //     name: 'Views / Visitor',
+  //     value: (json.pageviews.value / json.visitors.value).toFixed(2),
+  //   })
+  //   return stats
+  // })
 
   config.addPassthroughCopy({ public: './' })
   config.setBrowserSyncConfig({
@@ -90,47 +90,88 @@ module.exports = (config) => {
     return (str + '').split(sep)
   })
 
-  config.addNunjucksAsyncFilter('views', async function (posts, callback) {
-    let data = await fetch(
-      'https://plausible.io/api/v1/stats/breakdown?site_id=nsursock.netlify.app&period=6mo&property=event:page',
-      {
-        method: 'GET',
-        headers: {
-          // Authorization: `Bearer ${process.env.PLAUSIBLE_KEY}`,
-          Authorization: `Bearer 2K985BK3VVcjSZIpdUo6mbWti2118_DoDvoi9k2nxxZwMPoOmrYFYpARCPUhbR97`,
-        },
-      }
-    )
-    let json = (await data.json()).results
-    let views = json?.filter((item) => item.page.includes('/blog/'))
+  // config.addNunjucksAsyncFilter('views', async function (posts, callback) {
+  //   let data = await fetch(
+  //     'https://plausible.io/api/v1/stats/breakdown?site_id=nsursock.netlify.app&period=6mo&property=event:page',
+  //     {
+  //       method: 'GET',
+  //       headers: {
+  //         // Authorization: `Bearer ${process.env.PLAUSIBLE_KEY}`,
+  //         Authorization: `Bearer 2K985BK3VVcjSZIpdUo6mbWti2118_DoDvoi9k2nxxZwMPoOmrYFYpARCPUhbR97`,
+  //       },
+  //     }
+  //   )
+  //   let json = (await data.json()).results
+  //   let views = json?.filter((item) => item.page.includes('/blog/'))
 
-    callback(
-      null,
-      views?.map((view) => {
-        const index = posts.findIndex((post) => post.url === view.page)
-        if (index !== -1) return { post: posts[index], views: view.visitors }
-      })
-    )
-  })
+  //   callback(
+  //     null,
+  //     views?.map((view) => {
+  //       const index = posts.findIndex((post) => post.url === view.page)
+  //       if (index !== -1) return { post: posts[index], views: view.visitors }
+  //     })
+  //   )
+  // })
+
+  // config.addNunjucksAsyncFilter('top', async function (posts, callback) {
+  //   let data = await fetch(
+  //     'https://plausible.io/api/v1/stats/breakdown?site_id=nsursock.netlify.app&period=6mo&property=event:page',
+  //     {
+  //       method: 'GET',
+  //       headers: {
+  //         // Authorization: `Bearer ${process.env.PLAUSIBLE_KEY}`,
+  //         Authorization: `Bearer 2K985BK3VVcjSZIpdUo6mbWti2118_DoDvoi9k2nxxZwMPoOmrYFYpARCPUhbR97`,
+  //       },
+  //     }
+  //   )
+  //   let json = (await data.json()).results
+  //   let tops = json?.filter((item) => item.page.includes('/blog/')).slice(0, 3)
+
+  //   callback(
+  //     null,
+  //     tops?.map((top) => {
+  //       const index = posts.findIndex((post) => post.url === top.page)
+  //       if (index !== -1) return posts[index]
+  //     })
+  //   )
+  // })
 
   config.addNunjucksAsyncFilter('top', async function (posts, callback) {
+    let token = await fetch(
+      'https://statumami-production.up.railway.app/api/auth/login',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: 'admin',
+          password: statPwd,
+        }),
+      }
+    )
+    token = (await token.json()).token
+
+    let endAt = new Date().getTime()
+      startAt = new Date().getTime() - (7 * 24 * 60 * 60 * 1000)
+
     let data = await fetch(
-      'https://plausible.io/api/v1/stats/breakdown?site_id=nsursock.netlify.app&period=6mo&property=event:page',
+      `https://statumami-production.up.railway.app/api/website/1/metrics?start_at=${startAt}&end_at=${endAt}&type=url`,
       {
         method: 'GET',
         headers: {
-          // Authorization: `Bearer ${process.env.PLAUSIBLE_KEY}`,
-          Authorization: `Bearer 2K985BK3VVcjSZIpdUo6mbWti2118_DoDvoi9k2nxxZwMPoOmrYFYpARCPUhbR97`,
+          Authorization: `Bearer ${token}`,
         },
       }
     )
-    let json = (await data.json()).results
-    let tops = json?.filter((item) => item.page.includes('/blog/')).slice(0, 3)
+    let json = await data.json()
+    let tops = json?.filter((item) => item.x.includes('/blog/')).slice(0, 3)
 
     callback(
       null,
       tops?.map((top) => {
-        const index = posts.findIndex((post) => post.url === top.page)
+        const index = posts.findIndex((post) => post.url === top.x)
         if (index !== -1) return posts[index]
       })
     )
--- src/_data/env.js
+++ src/_data/env.js
@@ -3,4 +3,5 @@ module.exports = {
   devMode: process.env.NODE_ENV !== 'production',
   environment: process.env.NODE_ENV || 'development',
   stripeKey: process.env.ELEVENTY_STRIPE_KEY,
+  statPwd: process.env.UMAMI_ADMIN_PWD,
 }
