b0571234c1234bcf54ea00a9c51db12046f23527
Nicolas Sursock <nicolas.sursock@gmail.com>
2022-06-02 14:48:40 +0200

    changed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,39 +10,6 @@ module.exports = {
       position: ['bottom', 'right'],
     },
     extend: {
-      // typography(theme) {
-      //   return {
-      //     dark: {
-      //       css: {
-      //         color: theme('colors.gray.300'),
-      //         '[class~="lead"]': { color: theme('colors.gray.400') },
-      //         a: { color: theme('colors.gray.100') },
-      //         strong: { color: theme('colors.gray.100') },
-      //         'ul > li::before': { backgroundColor: theme('colors.gray.700') },
-      //         hr: { borderColor: theme('colors.gray.800') },
-      //         blockquote: {
-      //           color: theme('colors.gray.100'),
-      //           borderLeftColor: theme('colors.gray.800'),
-      //         },
-      //         h1: { color: theme('colors.gray.100') },
-      //         h2: { color: theme('colors.gray.100') },
-      //         h3: { color: theme('colors.gray.100') },
-      //         h4: { color: theme('colors.gray.100') },
-      //         code: { color: theme('colors.gray.100') },
-      //         'a code': { color: theme('colors.gray.100') },
-      //         pre: {
-      //           color: theme('colors.gray.200'),
-      //           backgroundColor: theme('colors.gray.800'),
-      //         },
-      //         thead: {
-      //           color: theme('colors.gray.100'),
-      //           borderBottomColor: theme('colors.gray.700'),
-      //         },
-      //         'tbody tr': { borderBottomColor: theme('colors.gray.800') },
-      //       },
-      //     },
-      //   }
-      // },
       fontFamily: {
         // erica: ['Erica One', 'cursive'],
         //   poppins: ['Poppins', 'sans-serif']
@@ -51,7 +18,8 @@ module.exports = {
         // heading: ['Baloo Da 2', 'cursive'],
         // paragraph: ['Lora', 'serif']
         // website: ['Work Sans', 'sans-serif'],
-        website: ['Special Elite', 'cursive'],
+        // website: ['Special Elite', 'cursive'],
+        website: ['Aldrich', 'sans-serif'],
       },
       animation: {
         'spin-slow': 'spin 3s linear infinite',
