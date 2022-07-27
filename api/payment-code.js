2cdd9b1e064520a57c6c876610af8af25bedc5e7
Nicolas Sursock <nicolas.sursock@gmail.com>
2022-07-27 17:19:05 +0200

    fixed bug in payment api with trailing numbers

--- api/payment-code.js
+++ api/payment-code.js
@@ -3,7 +3,7 @@ const stripe = require('stripe')(process.env.EXPRESS_STRIPE_KEY)
 export default async function handler(request, response) {
   
   const intent = await stripe.paymentIntents.create({
-    amount: parseFloat(request.body.protection) * 100,
+    amount: Math.round(parseFloat(request.body.protection) * 100),
     currency: 'eur',
     automatic_payment_methods: { enabled: true },
   })
