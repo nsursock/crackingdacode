383ae914fd58acc4612e5c3f7d67db77b3750189
Nicolas Sursock <nicolas.sursock@gmail.com>
2022-07-30 09:16:29 +0200

    added log of ip

---
+++ api/stats-ip-log.js
@@ -0,0 +1,25 @@
+import { createClient } from '@supabase/supabase-js'
+
+async function handler(request, response) {
+  const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+  )
+
+  const storageName = process.env.NODE_ENV.startsWith('dev')
+    ? 'sessions.dev'
+    : 'sessions'
+
+  try {
+    const { ipAddress } = request.body
+    await supabase.from(storageName).insert({
+      ip_address: ipAddress,
+    })
+    response.status(200).send({ success: true })
+  } catch (err) {
+    console.log(err)
+    response.status(400).send({ success: false })
+  }
+}
+
+export default handler
