eb0e80f676557111490cadc8530c99fdb6e55e3f
Nicolas Sursock <nicolas.sursock@gmail.com>
2022-07-31 08:38:21 +0200

    added returning users handler as a comment (vercel hobby limit)

--- api/stats-ip-log.js
+++ api/stats-ip-log.js
@@ -25,3 +25,31 @@ async function handler(request, response) {
 }
 
 export default handler
+
+// import { createClient } from '@supabase/supabase-js'
+
+// async function handler(request, response) {
+//   const supabase = createClient(
+//     process.env.SUPABASE_URL,
+//     process.env.SUPABASE_KEY
+//   )
+
+//   const storageName = process.env.NODE_ENV.startsWith('dev')
+//     ? 'sessions.dev'
+//     : 'sessions'
+
+//   try {
+//     const { startDate, endDate } = request.body
+//     const { data, error } = supabase
+//       .from(storageName)
+//       .select('*')
+//       .lt('created_at', new Date(endDate).toISOString())
+//       .gt('created_at', new Date(startDate).toISOString())
+//     response.status(200).json({ data })
+//   } catch (err) {
+//     console.log(err)
+//     response.status(400).send({ success: false })
+//   }
+// }
+
+// export default handler
